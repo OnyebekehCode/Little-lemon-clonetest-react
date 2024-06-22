2fcb8b415b1897b9e015dddc81ffdef4d75ce294
Sam0code <deemedx0@gmail.com>
2024-06-22 19:41:30 -0400

    added more updates

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "./Components/Header";
 import Nav from "./Components/Nav";
 import Main from "./Components/Main";
+import Footer from "./Components/Footer"
 function App() {
   return (
     <div className="App">
      <Header/>
      <Nav/>
-     <footer/>
      <Main/>
+     <Main h1="a" h2="b"/>
+     <Main h2="c" h3="d"/>
+     <Main h3="e" h1="f"/>
+     <Footer/>
     </div>
   );
 }
