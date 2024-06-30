3a34696d2d3e559e19c9d13c161c2e625c76545d
Onyebekehcode <deemedx0@gmail.com>
2024-06-30 14:10:29 -0400

    added mistakenly deleted header file

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Styles.css';
+
+function Header() {
+    const [isLightMode, setIsLightMode] = useState(true);
+
+    const handleClick = () => {
+        setIsLightMode(!isLightMode);
+
+        if (isLightMode) {
+            document.body.classList.add('dark-mode');
+            document.body.classList.remove('light-mode');
+        } else {
+            document.body.classList.add('light-mode');
+            document.body.classList.remove('dark-mode');
+        }
+    };
+
+    return (
+        <>
+            <div>
+                <h1>Little Lemon Restaurant</h1>
+                <img src="Logo.jpg" alt="Logo" className="logo" />
+            </div>
+            <button className='darkmode' onClick={handleClick}>
+                {isLightMode ? "Dark Mode" : "Light Mode"}
+            </button>
+        </>
+    );
+}
+
+export default Header;
